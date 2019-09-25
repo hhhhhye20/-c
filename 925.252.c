f5689c9d1baa10fb0afa4174387fb52af20b4454
hhhhhye20 <46595624+hhhhhye20@users.noreply.github.com>
2019-09-25 17:41:15 +0900

    Add files via upload
    재귀함수 간단한 문제. 2의n승 구하는 문제.

--- 925.252.c
+++ 925.252.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
+
+int func(int k, int j)
+{
+
+
+	j *= 2; // j==2
+
+	if(k==1)
+		return j;
+
+	func(k-1, j); //k==7. 2의1승=2니까, 지금 1을 써서 j가 2가 된거..!..
+
+
+	/* int i=2;
+
+	i *= 2; //i==4
+
+	if(k==0)
+		return i;
+
+	func(k-1); //k=7을 다시 매개변수로, 함수 호출
+	*/
+}
+
+
 int main(void)
 {
-	//숫자n입력받음. 다음 공식이 성립하는 k의 최댓값. 2의 k승 <= n
-
-	int n, k=0;
-	int i=2;
-
-	printf("상수 n 입력: ");
+	int n;
+	
+	printf("정수입력 : ");
 	scanf("%d", &n);
 
-/*	while(i<=n )
-	{
-		i *= 2;
-		k++;
-	}
-	*/
+	printf("2의 8승은 %d\n", func(n, 1));
 
-	for(i=2; i<=n; i*=2)
-	{
-		k++;
-	}
-
-	printf("공식을 만족하는 k의 값: %d\n", k);
 
 	return 0;
 	
